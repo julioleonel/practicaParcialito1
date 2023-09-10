989a4111cdfb5e52975e82a5484fee1ca2146933
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-10 11:33:48 -0300

    Agrego codigo

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -2,6 +2,22 @@ package ar.edu.undef.fie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        if (args.length == 0) {
+            System.out.println("No se proporcionaron argumentos");
+            return;
+        }
+
+            System.out.println("Ingreso los siguientes argumentos: ");
+            for (String argumento : args){
+                System.out.println(argumento + " ");
+            }
+
+        int cantidad = args.length;
+
+        System.out.println("La frase tiene: " + cantidad + " palabras");
+
     }
 }
+
+
